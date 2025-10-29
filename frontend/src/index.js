4fbffb9cfad67a36c3332d60128507af44e0dc51
Chikamso Chidebe <chikamsochidebe@gmail.com>
2025-10-29 03:36:38 +0100

    ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import './index.css';
 import App from './App';
 import { store } from './store/store';
@@ -13,7 +12,6 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
-        <Toaster position="top-right" />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
